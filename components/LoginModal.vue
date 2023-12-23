cd089be265b550e858ca2130f3d57d525632c00a
iamsouliya <souliyatmv24@gmail.com>
2023-12-24 00:15:37 +0700

    feat: google login

--- components/LoginModal.vue
+++ components/LoginModal.vue
@@ -28,42 +28,44 @@ async function signIn() {
 
 <template>
   <UModal v-model="login.showModal">
-    <UCard class="max-w-xl w-full">
-      <TransitionGroup name="slide-left">
-        <div key="1" class="">
-          <h1 class="text-center font-semibold text-xl">
-            Welcome to Skillhive!
-          </h1>
-
-          <div class="flex flex-col gap-2">
-            <UButton leading-icon="i-mdi-google" block @click="status = 'Google'">
-              <div class="flex text-center">
-                Continue with Google
-              </div>
-            </UButton>
-            <UButton icon="i-mdi-google" block @click="status = 'Email'">
-              Continue with Email
-            </UButton>
+    <UCard class="max-w-xl min-h-[400px] w-full">
+      <section class="grid grid-cols-2 h-full">
+        <article>1</article>
+        <article class="flex flex-col h-full">
+          <div key="1" class="">
+            <h1 class="text-center font-semibold text-xl">
+              Welcome to Skillhive!
+            </h1>
+            <div class="flex flex-col gap-2">
+              <UButton leading-icon="i-mdi-google" block @click="status = 'Google'">
+                <div class="flex text-center">
+                  Continue with Google
+                </div>
+              </UButton>
+              <UButton icon="i-mdi-google" block @click="status = 'Email'">
+                Continue with Email
+              </UButton>
+            </div>
           </div>
-        </div>
-        <UForm v-if="status === 'Email'" key="2" :state="state" class="space-y-3">
-          <div>
-            <UFormGroup label="Email">
-              <UInput v-model="state.email" type="email" />
-            </UFormGroup>
-          </div>
-          <div>
-            <UFormGroup label="Password">
-              <UInput v-model="state.password" type="password" />
-            </UFormGroup>
-          </div>
-          <div>
-            <UButton :loading="loading" @click="signIn">
-              Login
-            </UButton>
-          </div>
-        </UForm>
-      </TransitionGroup>
+          <UForm v-if="status === 'Email'" key="2" :state="state" class="space-y-3">
+            <div>
+              <UFormGroup label="Email">
+                <UInput v-model="state.email" type="email" />
+              </UFormGroup>
+            </div>
+            <div>
+              <UFormGroup label="Password">
+                <UInput v-model="state.password" type="password" />
+              </UFormGroup>
+            </div>
+            <div>
+              <UButton :loading="loading" @click="signIn">
+                Login
+              </UButton>
+            </div>
+          </UForm>
+        </article>
+      </section>
     </UCard>
   </UModal>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -11,26 +11,44 @@ const state = ref({
 })
 const loading = ref(false)
 
-async function signIn() {
+async function signIn(provider?: 'google' | 'github' | 'facebook') {
   loading.value = true
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: state.value.email,
-    password: state.value.password,
-  })
-  if (error?.message) {
-    loading.value = false
-    return console.error(error.message)
+  if (provider) {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: 'http://localhost:3000',
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    })
+    if (error?.message) {
+      loading.value = false
+      return console.error(error.message)
+    }
+  }
+  else {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: state.value.email,
+      password: state.value.password,
+    })
+    if (error?.message) {
+      loading.value = false
+      return console.error(error.message)
+    }
+    if (data.user)
+      navigateTo('/')
   }
 
   loading.value = false
-  if (data.user)
-    navigateTo('/')
 }
 </script>
 
 <template>
-  <div class="flex h-full w-full  flex-col items-center justify-center">
-    <UCard class="max-w-xl w-full">
+  <div class="flex min-h-screen w-full items-center justify-center">
+    <UCard class="sm:w-full sm:max-w-xl">
       <section class="space-y-3">
         <h1 class="text-center font-semibold text-xl">
           Welcome to Skillhive!
@@ -48,6 +66,20 @@ async function signIn() {
         <div>
           <UButton :loading="loading" @click="signIn">
             Login
+          </UButton>
+        </div>
+        <div class="text-center">
+          <ULink href="/signup">
+            Don't have an account? Sign up
+          </ULink>
+        </div>
+        <UDivider label="Or continue with" />
+        <div class="space-y-1">
+          <UButton variant="outline" block @click="signIn('google')">
+            <UIcon name="i-mdi-google" class="text-xl" />
+          </UButton>
+          <UButton variant="outline" block @click="signIn('github')">
+            <UIcon name="i-mdi-facebook" class="text-xl" />
           </UButton>
         </div>
       </section>
