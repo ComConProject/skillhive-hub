5c03865044694d8a7083d2ae164942bdad889fbb
iamsouliya <souliyatmv24@gmail.com>
2024-01-29 16:30:32 +0700

    feat: ongoing category page

---
+++ components/TheCategory.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="">
+    Hello
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,9 @@
     <nav class="text-sm sm:mt-4 sm:px-6 sm:text-base xl:pt-0 2xl:pt-0">
       <TheNav class="hidden sm:block" />
     </nav>
+    <section class="text-sm sm:mt-4 sm:px-6 sm:text-base xl:pt-0 2xl:pt-0">
+      <TheCategory />
+    </section>
     <main class="flex flex-1 flex-col overflow-auto">
       <div class="text-sm sm:mt-4 sm:px-6 sm:text-base xl:pt-0 2xl:pt-0">
         <slot />
