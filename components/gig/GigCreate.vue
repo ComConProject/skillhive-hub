edc9c0093b093f7bb3f8b5063739e3492eefb07c
iamsouliya <souliyatmv24@gmail.com>
2024-05-26 17:23:52 +0700

    feat:ongoing pricing table

--- components/gig/GigCreate.vue
+++ components/gig/GigCreate.vue
@@ -72,7 +72,62 @@ function useInlineGig() {
               </UFormGroup>
             </div>
           </section>
-          <section v-show="step === 2" key="2" />
+          <section v-show="step === 2" key="2">
+            <!-- pricing table that has basic, standard, and premium -->
+            <div class=" text-3xl col-span-2 font-bold text-slate-800 mb-4">
+              Package
+            </div>
+            <table class="table-auto w-full text-left">
+              <thead
+                class="border-y border-slate-100 dark:border-slate-800"
+              >
+                <tr>
+                  <th class="px-2 py-1.5">
+&nbsp;
+                  </th>
+                  <th class="px-2 py-1.5">
+                    Basic
+                  </th>
+                  <th class="px-2 py-1.5">
+                    Standard
+                  </th>
+                  <th class="px-2 py-1.5">
+                    Premium
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-slate-100 dark:divide-slate-800">
+                <tr>
+                  <td class="px-2 py-1.5">
+                    sss
+                  </td>
+                  <td class="px-2 py-1.5">
+                    name
+                  </td>
+                  <td class="px-2 py-1.5">
+                    name
+                  </td>
+                  <td class="px-2 py-1.5">
+                    name
+                  </td>
+                </tr>
+                <tr>
+                  <td class="px-2 py-1.5">
+                    description
+                  </td>
+                  <td class="px-2 py-1.5">
+                    description
+                  </td>
+                  <td class="px-2 py-1.5">
+                    description
+                  </td>
+                  <td class="px-2 py-1.5">
+                    description
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
         </TransitionGroup>
         <div class="flex gap-1 mt-4 items-center col-span-2">
           <UButton v-if="step === 1" variant="outline" @click="$router.back()">
