6b4d96f056dca24c54ca4ccecca6f1af5ad92cfa
iamsouliya <souliyatmv24@gmail.com>
2024-06-13 15:26:01 +0700

    feat: change to use stripe module

--- server/api/products/index.get.ts
+++ server/api/products/index.get.ts
@@ -1,12 +1,7 @@
-import process from 'node:process'
-import { defineEventHandler } from 'h3'
-import { Stripe } from 'stripe'
+import { useServerStripe } from '#stripe/server'
 
 export default defineEventHandler(async (event) => {
-//   const config = useRuntimeConfig()
-  const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
-    apiVersion: '2022-11-15',
-  })
+  const stripe = await useServerStripe(event)
 
   const products = await stripe.products.list(
     {
