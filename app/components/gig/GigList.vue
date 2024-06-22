1987541ae960af29b9ec189eb2da8716655dcb10
iamsouliya <souliyatmv24@gmail.com>
2024-06-22 22:42:28 +0700

    remove $

--- app/components/gig/GigList.vue
+++ app/components/gig/GigList.vue
@@ -15,7 +15,7 @@ function formatGigForCard(gigs: Pricing[]) {
       img: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       createdAtLabel: d(new Date(gig.created_at), 'short'),
       disabled: false,
-      offer: `$${gig.price}`,
+      offer: `${gig.price}`,
       ownerLabel: `${gig.providing_service?.description}`,
     }
   })
