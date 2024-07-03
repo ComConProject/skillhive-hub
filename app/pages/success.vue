35ecadf2e90696994d4fc8b642e458cc408b956e
iamsouliya <souliyatmv24@gmail.com>
2024-07-03 18:55:46 +0700

    feat: success page

--- app/pages/success.vue
+++ app/pages/success.vue
@@ -14,11 +14,30 @@ async function getStripeSession() {
   }
 }
 
-getStripeSession()
+if (route.query.session_id) {
+  await getStripeSession()
+}
 </script>
 
 <template>
-  <div>
-    Hello
-  </div>
+  <UCard class="mx-auto max-w-3xl">
+    <section class="text-center flex items-center justify-center flex-col space-y-3">
+      <div>
+        <NuxtImg src="/successful_purchase.svg" alt="Successful purchase" width="300" height="300" />
+      </div>
+      <div>
+        <Icon name="material-symbols:check-circle-outline" size="50" class="text-green-500" />
+      </div>
+      <h1 class="text-3xl font-bold">
+        {{ $t('successful_purchase') }}
+      </h1>
+
+      <!-- button to go to order page -->
+      <div>
+        <UButton to="/orders" icon="i-heroicons-arrow-up-right">
+          {{ $t('go_to_orders') }}
+        </UButton>
+      </div>
+    </section>
+  </UCard>
 </template>
